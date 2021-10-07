f06605361aeac73ccc5a12a203b5717be655de50
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-10-07 16:51:13 -0400

    contact me

--- pages/Contact.js
+++ pages/Contact.js
@@ -4,9 +4,18 @@ export default function Contact(props) {
    return (
       <div>
         <Nav />
-        <h1>Contact Me</h1>
-        <img src='/me2.jpeg' alt=''/>
-        <a href="mailto:swellmerdev@gmail.com?&subject=Let's get in touch!" target="_blank" rel="noreferrer">Email me!</a>
+        <div className='contact'>
+         <h1>Contact Me</h1>
+         <div className='contactPhoto'>
+         <h1 className='hi'>Hi, I'm Samantha</h1>
+         <img src='/me2.jpeg' alt='girl sitting on a tree stump in front of a pub'/>
+        </div>
+        <div className='buttons'>
+         <button><a href="mailto:swellmerdev@gmail.com?&subject=Let's get in touch!" target="_blank" rel="noreferrer">Email me!</a></button>
+         <button><a href='https://www.linkedin.com/in/samanthawellmer/'>LinkedIn</a></button>
+         <button><a href='https://www.instagram.com/splendidsam/'>Instagram</a></button>
+        </div>
+     </div>
      </div>
     )
   }
